dc6ad63593ed90f63d1bb8e4ba763b46411da7ca
yegor <astery227@gmail.com>
2024-11-13 19:09:06 +0300

    add apitostring log message

--- template-client/src/features/todos/api/fetchPagedTodos.ts
+++ template-client/src/features/todos/api/fetchPagedTodos.ts
@@ -2,9 +2,7 @@ import PagedTodoResponse from "@/features/todos/types/PagedTodoResponse";
 import api from "@/lib/api/api";
 
 export default async function fetchPagedTodos({ pageParam = 1 }) {
-    console.log("process.env.api", process.env.api);
-    console.log("process.env.auth", process.env.auth);
-    console.log("process.env.cms", process.env.cms);
     const { data } = await api.get<PagedTodoResponse>(`todos/paged/${pageParam}`);
+    console.log("apiToString", api.toString());
     return data;
 }
