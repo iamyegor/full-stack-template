e93b9fdd8ef8a4370763c938263be0d15a1334f1
yegor <astery227@gmail.com>
2024-11-13 18:36:37 +0300

    make middleware unreachable by changing matcher

--- template-client/src/middleware.ts
+++ template-client/src/middleware.ts
@@ -4,5 +4,5 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-    matcher: ["/", "/(ru|en)/:path*"],
+    matcher: ["/289314981984981"],
 };
