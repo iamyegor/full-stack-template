625e19d0a724c908089712a1f2df18dc07ea2126
yegor <astery227@gmail.com>
2024-11-13 18:56:27 +0300

    change middleware back to the initial state

--- template-client/src/middleware.ts
+++ template-client/src/middleware.ts
@@ -4,5 +4,5 @@ import { routing } from "./i18n/routing";
 export default createMiddleware(routing);
 
 export const config = {
-    matcher: ["/289314981984981"],
+    matcher: ["/", "/((?!_next).*)"],
 };
