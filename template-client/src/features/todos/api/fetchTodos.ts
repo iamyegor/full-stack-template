5ce5991d0d04bf690f40eaf95fcf08761d09c3b7
yegor <astery227@gmail.com>
2024-11-14 19:46:14 +0300

    fix fetch todos request route

--- template-client/src/features/todos/api/fetchTodos.ts
+++ template-client/src/features/todos/api/fetchTodos.ts
@@ -3,6 +3,6 @@
 import { Todo } from "@/features/todos/types/Todo";
 
 export default async function fetchTodos(): Promise<Todo[]> {
-    const response = await fetch(process.env.api + "todos");
+    const response = await fetch(process.env.api + "/todos");
     return (await response.json()) as Todo[];
 }
